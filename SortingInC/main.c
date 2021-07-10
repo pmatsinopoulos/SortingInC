13edc0838cb1649f1b308d7cd037dd6aa3e94b67
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-10 16:15:28 +0300

    Initial Commit

---
+++ SortingInC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SortingInC
+//
+//  Created by Panayotis Matsinopoulos on 10/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
